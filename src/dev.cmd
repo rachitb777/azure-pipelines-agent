c223a06892528d899ee0029b1a070698c079997d
Tommy Petty <jopetty@microsoft.comwq>
2020-01-13 14:26:15 -0500

    Trying something else

--- src/dev.cmd
+++ src/dev.cmd
@@ -23,8 +23,8 @@ echo Unable to resolve location of sh.exe. 1>&2
 exit /b 1
 
 :run
-echo "##vso[task.prependpath]C:\Program Files\Git\mingw64\bin"
-echo "##vso[task.prependpath]C:\Program Files\Git\usr\bin"
-echo "##vso[task.prependpath]C:\Program Files\Git\bin"
+echo ##vso[task.prependpath]"C:\Program Files\Git\mingw64\bin"
+echo ##vso[task.prependpath]"C:\Program Files\Git\usr\bin"
+echo ##vso[task.prependpath]"C:\Program Files\Git\bin"
 echo on
 "%SH_PATH%" "%~dp0dev.sh" %*
