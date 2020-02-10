719c9bf35c4583c094e4da0ddcefe9568795c336
Tommy Petty <jopetty@microsoft.com>
2020-02-10 15:51:47 -0500

    Fixed testing on windows

--- src/Test/L0/Worker/TaskRunnerL0.cs
+++ src/Test/L0/Worker/TaskRunnerL0.cs
@@ -67,7 +67,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Tests.Worker
         [Trait("Category", "Worker")]
         public void GetHandlerHostOnlyTests()
         {
-            var nodeData = new NodeHandlerData() { Platforms=new string[]{"windows", "linux", "osx" }};
+            var nodeData = new NodeHandlerData() { Platforms=new string[]{"linux", "osx" }};
             var nodeOnlyExecutionData = new ExecutionData();
             nodeOnlyExecutionData.Node = nodeData;
             var powerShell3Data = new PowerShell3HandlerData() { Platforms=new string[]{"windows"}};
