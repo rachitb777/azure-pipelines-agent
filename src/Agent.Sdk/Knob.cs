d18f69cff77daca884ba6753ded0d9e969ac0089
Tommy Petty <jopetty@microsoft.comwq>
2020-02-11 13:12:09 -0500

    Updates from review

--- src/Agent.Sdk/Knob.cs
+++ src/Agent.Sdk/Knob.cs
@@ -39,18 +39,16 @@ namespace Agent.Sdk
         }
     }
 
-    public class AgentKnobs : KnobPanel
+    public class AgentKnobs : ControlPanel
     {
-        public static readonly IKnob UseNode10 = new Knob(nameof(UseNode10))
+        public static readonly IKnob UseNode10 = new Knob(nameof(UseNode10), "false", "Forces the agent to use Node 10 handler for all Node-based tasks")
         {
             EnvironmentVariableNames = new List<string>{ "AGENT_USE_NODE10" },
-            RuntimeVariableNames = new List<string>{ "AGENT_USE_NODE10" },
-            Description = "Forces the agent to use Node 10 handler for all Node-based tasks",
-            DefaultValue = "false"
+            RuntimeVariableNames = new List<string>{ "AGENT_USE_NODE10" }
         };
     }
 
-    public class KnobPanel
+    public class ControlPanel
     {
 
         private static readonly IDictionary<string,IKnob> _knobs = new Dictionary<string,IKnob>();
@@ -61,14 +59,16 @@ namespace Agent.Sdk
             public List<string> EnvironmentVariableNames { get; set; }
             public List<string> RuntimeVariableNames { get; set; }
             // public TBDType CommandLineOption {get; private set;}
-            public string Description { get; set; }
+            public string Description { get; private set; }
             public bool IsDeprecated {get; set; } = false;  // is going away at a future date
             public bool IsExperimental {get; set; } = false; // may go away at a future date
-            public string DefaultValue {get; set; } = "";
+            public string DefaultValue {get; private set; }
 
-            public Knob(string name)
+            public Knob(string name, string defaultValue, string description)
             {
                 Name = name;
+                DefaultValue = defaultValue;
+                Description = description;
                 if (AgentKnobs._knobs.ContainsKey(name))
                 {
                     throw new ArgumentException($"Already have a knob called {name}", nameof(name));
@@ -78,6 +78,7 @@ namespace Agent.Sdk
 
             public KnobValue GetValue(IKnobValueContext context)
             {
+                ArgUtil.NotNull(context, nameof(context));
                 //TODO: First thing we need to do is check if deprecated or experimental and throw an event that
                 //      can be logged
 
@@ -86,32 +87,30 @@ namespace Agent.Sdk
                 // 2. Command Line Options (Not Yet Implemented)
                 // 3. Environment Variables
                 // 4. Default
-                if (context != null)
+                if (!(RuntimeVariableNames is null))
                 {
-                    if (!(RuntimeVariableNames is null))
+                    foreach (var runTimeVar in RuntimeVariableNames)
                     {
-                        foreach (var runTimeVar in RuntimeVariableNames)
+                        var value = context.GetVariableValueOrDefault(runTimeVar);
+                        if (value != null)
                         {
-                            var value = context.GetVariableValueOrDefault(runTimeVar);
-                            if (value != null)
-                            {
-                                return new KnobValue(value, KnobSource.RuntimeVariable, runTimeVar);
-                            }
-                        }
-                    }
-                    if (!(EnvironmentVariableNames is null))
-                    {
-                        var scopedEnvironment = context.GetScopedEnvironment();
-                        foreach (var envVar in EnvironmentVariableNames)
-                        {
-                            var value = scopedEnvironment.GetEnvironmentVariable(envVar);
-                            if (!string.IsNullOrEmpty(value))
-                            {
-                                return new KnobValue(value, KnobSource.EnvironmentVariable, envVar);
-                            }
+                            return new KnobValue(value, KnobSource.RuntimeVariable, runTimeVar);
                         }
                     }
                 }
+                if (!(EnvironmentVariableNames is null))
+                {
+                    var scopedEnvironment = context.GetScopedEnvironment();
+                    foreach (var envVar in EnvironmentVariableNames)
+                    {
+                        var value = scopedEnvironment.GetEnvironmentVariable(envVar);
+                        if (!string.IsNullOrEmpty(value))
+                        {
+                            return new KnobValue(value, KnobSource.EnvironmentVariable, envVar);
+                        }
+                    }
+                }
+
                 return new KnobValue(DefaultValue, KnobSource.BuiltInDefault);
             }
         }
