9c587f21ea515ef6edf7b1747e3dbd357d0fc620
damccorm <damccorm@microsoft.com>
2020-02-11 10:55:34 -0500

    Only execute l1s when called

--- src/dev.sh
+++ src/dev.sh
@@ -105,6 +105,17 @@ function cmd_test ()
     dotnet msbuild -t:test -p:PackageRuntime="${RUNTIME_ID}" -p:BUILDCONFIG="${BUILD_CONFIG}" -p:AgentVersion="${AGENT_VERSION}" -p:LayoutRoot="${LAYOUT_DIR}" -p:SkipOn="${CURRENT_PLATFORM}" || failed "failed tests"
 }
 
+function cmd_test_l0 ()
+{
+    heading "Testing L0"
+
+    if [[ ("$CURRENT_PLATFORM" == "linux") || ("$CURRENT_PLATFORM" == "darwin") ]]; then
+        ulimit -n 1024
+    fi
+
+    dotnet msbuild -t:testl0 -p:PackageRuntime="${RUNTIME_ID}" -p:BUILDCONFIG="${BUILD_CONFIG}" -p:AgentVersion="${AGENT_VERSION}" -p:LayoutRoot="${LAYOUT_DIR}" -p:SkipOn="${CURRENT_PLATFORM}" || failed "failed tests"
+}
+
 function cmd_test_l1 ()
 {
     heading "Testing L1"
@@ -283,6 +294,7 @@ case $DEV_CMD in
    "b") cmd_build;;
    "test") cmd_test;;
    "t") cmd_test;;
+   "testl0") cmd_test_l0;;
    "testl1") cmd_test_l1;;
    "layout") cmd_layout;;
    "l") cmd_layout;;
