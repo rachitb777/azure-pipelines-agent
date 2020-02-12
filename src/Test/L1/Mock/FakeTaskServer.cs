61f2c0a20dde99217e84039b7bf99181164944d2
damccorm <damccorm@microsoft.com>
2020-02-12 13:37:36 -0500

    Return task zip stream

--- src/Test/L1/Mock/FakeTaskServer.cs
+++ src/Test/L1/Mock/FakeTaskServer.cs
@@ -16,6 +16,18 @@ namespace Microsoft.VisualStudio.Services.Agent.Tests.L1.Worker
 
         public Task<Stream> GetTaskContentZipAsync(Guid taskId, TaskVersion taskVersion, CancellationToken token)
         {
+            String baseDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            String taskZipsPath = Path.Join(baseDirectory, "TaskZips");
+
+            foreach (String zip in Directory.GetFiles(taskZipsPath))
+            {
+                String zipName = Path.GetFileNameWithoutExtension(zip);
+                if (zipName.Equals(taskId.ToString()))
+                {
+                    return Task.FromResult<Stream>(new FileStream(zip, FileMode.Open));
+                }
+            }
+
             return Task.FromResult<Stream>(null);
         }
 
